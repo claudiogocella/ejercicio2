34340a0d2c086b2f5e12096820cddd20b0c1e24b
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-04-22 20:34:05 -0300

    Initial commit

---
+++ src/ejercico2.ts
@@ -0,0 +1,10 @@
+console.log("avanzar");
+console.log("rellenar")
+console.log("avanzar")
+console.log("rellenar")
+console.log("girar a la izquierda")
+console.log("avanzar")
+console.log("rellenar")
+console.log("avanzar")
+console.log("rellenar")
+
